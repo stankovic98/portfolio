b28ea345d40703ed30972d0c2fe8dda7f9c74e5c
antonio <antonio.stankovic77@gmail.com>
2022-04-08 14:43:20 +0200

    styling for project item

--- src/components/ProjectItem.vue
+++ src/components/ProjectItem.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="project">
-    <img src="https://picsum.photos/200/300" alt="Preview of project" />
+    <img src="https://picsum.photos/200" alt="Preview of project" />
     <div class="overlay">
       <h3>Project name</h3>
       <p>
+        <!-- vjerovatno bude trebalo a zamijeniti s RouterLinkom -->
         Description in a few words, <a href="#">blog post</a>, if you want to
         know more
       </p>
@@ -30,7 +31,7 @@ img {
 }
 .overlay {
   position: relative;
-  background-color: rgba(224, 235, 72, 0.747);
+  background-color: var(--secondary);
   grid-row: 1 / -1;
   grid-column: 1 / -1;
   display: grid;
@@ -39,9 +40,35 @@ img {
   grid-gap: 10px;
   padding: 20px;
   color: grey;
-  font-size: 1.2rem;
+  font-size: 1.4rem;
   transition: 0.3s;
   transform: translateY(100%);
+}
+
+.overlay h3 {
+  font-family: monospace;
+  color: var(--primary);
+  font-size: 1.6rem;
+}
+
+.overlay p {
+  color: white;
+}
+
+button {
+  padding: 10px 20px;
+  font-size: 1.2rem;
+  letter-spacing: 4px;
+  background: rgba(0, 0, 0, 0);
+  border: 3px solid white;
+  border-radius: 5px;
+  color: white;
+  text-transform: uppercase;
+}
+
+button:hover {
+  color: var(--primary);
+  background-color: white;
 }
 
 .project:hover .overlay {
